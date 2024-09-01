b5bc4fa2187f434d4dc0767b4cd279a98e1cffb7
unknown <shohidulislamsifat2003@gmail.com>
2024-09-01 19:51:03 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,11 @@
+const numbers =[23,45,5,58,56,67];
+
+console.log(numbers);
+for(const num of numbers ){
+    console.log(num);
+}
+const fruits =["apple","komla","amm","jam"];
+console.log(fruits);
+for(const frui of fruits){
+    console.log(frui)
+}
